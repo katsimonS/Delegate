11c2875ee54dd9cbfcaab1fe149450ec8ad46c44
Serhii Katsimon <jora.proforg@gmail.com>
2022-08-02 08:54:25 +0300

    Add project files.

---
+++ Delegate/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+//Create method for list sorting fuction (User{FirstName, LastN, DateOfBirthday, PhoneNumber})
+//  *Generic version
+//Create delegate which will allow to set Console.ReadLine()\WriteLine() inside it.
+//There is User{ FirstName,LastN,DateOfBirthday,PhoneNumber}. Create method to filter users list.
+//  *Create universal method to filter any collection (generic)
+//Create method which will repeat code part (should be as input parameter) while condition(should be as input parameter) is true N times.
